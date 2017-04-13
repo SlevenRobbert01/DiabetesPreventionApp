aa87b613734deb1ad7a3407a081f860a5c77498b
System Administrator <root@7c40kx1.stad.antwerpen.local>
2017-04-13 10:00:15 +0200

    material icons added

--- src/forms/screening.js
+++ src/forms/screening.js
@@ -3,8 +3,23 @@ import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import Subheader from 'material-ui/Subheader';
+import Checkbox from 'material-ui/Checkbox';
+import RaisedButton from 'material-ui/RaisedButton';
+import AlarmIcon from 'react-material-icons/icons/action/done';
+
 const style = {
   marginLeft: 20
+};
+
+const styles = {
+  checkbox: {
+    marginBottom: 16,
+    marginTop: 16,
+    marginLeft: 20
+  },
+  button: {
+    margin: 20,
+  }
 };
 
 class Screening extends Component {
@@ -12,14 +27,24 @@ class Screening extends Component {
     return (
       <Paper zDepth={2}>
         <Subheader>{'Screening'}</Subheader>
-        <TextField hintText="First name" style={style} underlineShow={false} />
+        <TextField floatingLabelText="Leeftijd" floatingLabelFixed={true} style={style} underlineShow={false} />
         <Divider />
-        <TextField hintText="Middle name" style={style} underlineShow={false} />
+        <TextField floatingLabelText="Gewicht in kg" floatingLabelFixed={true} style={style} underlineShow={false} />
         <Divider />
-        <TextField hintText="Last name" style={style} underlineShow={false} />
+        <TextField floatingLabelText="Lengte in cm" floatingLabelFixed={true} style={style} underlineShow={false} />
         <Divider />
-        <TextField hintText="Email address" style={style} underlineShow={false} />
+        <Checkbox label="Rookt u?" style={styles.checkbox} />
         <Divider />
+        <Checkbox label="Beweegt u veel?" style={styles.checkbox} />
+        <Divider />
+        <Checkbox label="Neemt u medicatie voor verhoogde bloeddruk?" style={styles.checkbox} />
+        <Divider />
+        <RaisedButton 
+          label="Start screening"
+          primary={true}
+          style={styles.button}
+          icon={<AlarmIcon/>}
+        />
       </Paper>
     );
   }
